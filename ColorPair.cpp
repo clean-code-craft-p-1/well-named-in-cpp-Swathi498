4f8c2f3833e0c3d1f0d8de0fa35677e540e0aa71
Swathi498 <swatisri.r@philips.com>
2023-07-31 22:19:37 +0530

    “ass2”

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -37,17 +37,17 @@ namespace TelCoColorCoder
 
 	void printRefManual()
 	{
-		std::cout << "Color Pair to Pair Number Reference Manual";
+		std::cout << "Color   Code   Manual \n";
 
-		int pairNo = 0;
+		int Pair = 0;
 
-		for (int majorCol = 0; majorCol < numberOfMajorColors; majorCol++) {
-			for (int minorCol = 0; minorCol < numberOfMinorColors; minorCol++) {
-				pairNo = GetPairNumberFromColor(static_cast<MajorColor>(majorCol), static_cast<MinorColor>(minorCol));
+		for (int majorCol = 0; majorCol < numberOfMajorColors; majorCol++) 
+		{
+			for (int minorCol = 0; minorCol < numberOfMinorColors; minorCol++) 
+			{
+				Pair = GetPairNumberFromColor(static_cast<MajorColor>(majorCol), static_cast<MinorColor>(minorCol));
 
-				std::cout << "Major Color : " << MajorColorNames[majorCol] << ", " << "Minor Color : "
-					<< MinorColorNames[minorCol] << " = " << pairNo << std::endl;
-				std::cout << "------------------------------------------------------";
+				std::cout << MajorColorNames[majorCol] << " " << MinorColorNames[minorCol] << " " << Pair << std::endl;
 			}
 		}
 	}
